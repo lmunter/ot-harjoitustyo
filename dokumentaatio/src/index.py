c3d465321611e82217a8f799aa9f336d95507809
lmunter <lassi.munter@gmail.com>
2022-12-05 01:25:28 +0200

    monta lisäystä

--- dokumentaatio/src/index.py
+++ dokumentaatio/src/index.py
@@ -11,23 +11,36 @@ class Pelaaja(pygame.sprite.Sprite):
         self.surf = pygame.image.load(self.image)
         self.surf.set_colorkey((255, 255, 255))
         self.rect = self.surf.get_rect()
+        self.rect.x = 50
+        self.rect.y = 980
+    
+    def hyppy(self):
+        hyppy = True
+        while hyppy:
+            while self.rect.y > 780:
+                self.rect.y -= 1
+            hyppy = False
 
 class Alusta(pygame.sprite.Sprite):
     def __init__(self):
         super(Alusta, self).__init__()
-        self.leveys = random.randint(300, 700)
+        self.leveys = random.randint(1300, 1700)
         self.surf = pygame.Surface((self.leveys, 50))
         self.surf.fill((10, 10, 10))
         self.rect = self.surf.get_rect()
+        self.rect.x = 0
+        self.rect.y = 1030
 
 class Peli:
     def __init__(self):
         pygame.init()
         pelaaja = Pelaaja()
         alusta = Alusta()
+        alusta2 = Alusta()
+        alusta2.rect.x = alusta.leveys + 100
 
-        screen = pygame.display.set_mode([500, 500])
-        pygame.display.set_caption("Maikkeli rotkoon")
+        screen = pygame.display.set_mode([1920, 1080])
+        pygame.display.set_caption("Skeittaaja rotkoon")
 
         running = True
         while running:
@@ -38,11 +51,32 @@ class Peli:
                 elif tapahtuma.type == pygame.KEYDOWN:
                     if tapahtuma.key == pygame.K_ESCAPE:
                         running = False
+                    elif tapahtuma.key == pygame.K_SPACE:
+                        if voi_hypata:
+                            pelaaja.hyppy()
             
-    
+            #painovoima
+            sprite_list = [pelaaja]
+            if pelaaja not in pygame.sprite.spritecollide(alusta, sprite_list, False) \
+            and pelaaja not in pygame.sprite.spritecollide(alusta2, sprite_list, False):
+                voi_hypata = False
+                pelaaja.rect.y += 1  
+            else:
+                voi_hypata = True
+            
+            alusta.rect.x -= 1
+            if alusta.rect.x == -(alusta.leveys):
+                alusta = Alusta()
+                alusta.rect.x = alusta2.leveys + 200
+            alusta2.rect.x -= 1
+            if alusta2.rect.x == -(alusta2.leveys):
+                alusta2 = Alusta()
+                alusta2.rect.x = alusta.leveys + 200
+
             screen.fill((235, 235, 235))
             screen.blit(pelaaja.surf, pelaaja.rect)
             screen.blit(alusta.surf, alusta.rect)
+            screen.blit(alusta2.surf, alusta2.rect)
             pygame.display.flip()
 
         pygame.quit()
