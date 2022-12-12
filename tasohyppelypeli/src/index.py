e046fe1dbdc90097d81ae006771f27e5a452ef21
lmunter <lassi.munter@gmail.com>
2022-12-12 17:42:05 +0200

    lisätty häviämisehto ja aloitussivu

--- tasohyppelypeli/src/index.py
+++ tasohyppelypeli/src/index.py
@@ -5,33 +5,69 @@ from player import Player
 class Game:
     def __init__(self):
         pygame.init()
+        self.font = pygame.font.SysFont("Arial", 24)
 
-        #luodaan oliot
+        self.screen = pygame.display.set_mode([1920, 1080])
+        pygame.display.set_caption("Skeittaaja rotkoon")
+        self.main_menu()
+
+    def main_menu(self):
+        main_menu_text = self.font.render("Skeittaaja rotkoon", True, (0, 0, 0))
+        main_menu_instructions = self.font.render("SPACE - Uusi peli    ESC - Sulje peli", True, (0, 0, 0))
+        self.screen.fill((235, 235, 235))
+        self.screen.blit(main_menu_text, (960, 340))
+        self.screen.blit(main_menu_instructions, (960, 440))
+        pygame.display.flip()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                    elif event.key == pygame.K_SPACE:
+                        self.new_game()
+
+    def new_game(self):
         self.player = Player()
         self.platform = Platform()
         self.platform2 = Platform()
         self.platform2.rect.x = self.platform.width + 100
 
-        screen = pygame.display.set_mode([1920, 1080])
-        pygame.display.set_caption("Skeittaaja rotkoon")
-
-        #pelisilmukka
-        self.running = True
-        while self.running:
+        while True:
             self.check_events()
 
             self.gravity(self.player, self.platform, self.platform2)
 
             self.platform.rect.x -= 1
             self.platform2.rect.x -= 1
+            self.recycle_platforms()
 
-            screen.fill((235, 235, 235))
-            screen.blit(self.player.surf, self.player.rect)
-            screen.blit(self.platform.surf, self.platform.rect)
-            screen.blit(self.platform2.surf, self.platform2.rect)
+            self.screen.fill((235, 235, 235))
+            self.screen.blit(self.player.surf, self.player.rect)
+            self.screen.blit(self.platform.surf, self.platform.rect)
+            self.screen.blit(self.platform2.surf, self.platform2.rect)
             pygame.display.flip()
 
-        pygame.quit()
+            if self.player.rect.y >= 1080:
+                self.game_over()
+
+    def game_over(self):
+        self.screen.fill((235, 235, 235))
+        game_over_text = self.font.render("Game Over", True, (0, 0, 0))
+        game_over_instructions = self.font.render("SPACE - Main menu    ESCAPE - Sulje peli", True, (0, 0, 0))
+        self.screen.blit(game_over_text, (960, 440))
+        self.screen.blit(game_over_instructions, (960, 540))
+        pygame.display.flip()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                    elif event.key == pygame.K_SPACE:
+                        self.main_menu()
 
     def recycle_platforms(self):
         if self.platform.rect.x == -(self.platform.width):
@@ -41,24 +77,26 @@ class Game:
             self.platform2 = Platform()
             self.platform2.rect.x = self.platform.width + 200
 
-
     def check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.running = False
+                pygame.quit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    self.running = False
-                elif event.key == pygame.K_SPACE:
-                    if self.can_jump:
-                        self.player.jump()
+                self.check_pressed_keys(event)
+
+    def check_pressed_keys(self, event):
+        if event.key == pygame.K_ESCAPE:
+            pygame.quit()
+        elif event.key == pygame.K_SPACE:
+            if self.can_jump:
+                self.player.jump()
 
     def gravity(self, player, platform1, platform2):
         sprite_list = [player]
         if player not in pygame.sprite.spritecollide(platform1, sprite_list, False) \
         and player not in pygame.sprite.spritecollide(platform2, sprite_list, False):
             self.can_jump = False
-            self.player.rect.y += 1
+            self.player.rect.y += 2
         else:
             self.can_jump = True
 
