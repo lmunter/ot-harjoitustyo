c38284772a4b6e7303f1f11270b256c4efbeee41
lmunter <lassi.munter@gmail.com>
2022-12-13 16:51:59 +0200

    test gravity

--- tasohyppelypeli/src/tests/index_test.py
+++ tasohyppelypeli/src/tests/index_test.py
@@ -1,16 +1,14 @@
-#En saanut näitä testejä vielä kuntoon
+import unittest
+from index import Game
+from my_platform import Platform
+from player import Player
 
-#import unittest
-#from index import Game
-#from my_platform import Platform
-#from player import Player
-
-#class TestGame(unittest.TestCase):
-#    def setUp(self):
-#        self.platform1 = Platform()
-#        self.platform2 = Platform()
-#        self.player = Player()
+class TestGame(unittest.TestCase):
+    def setUp(self):
+        self.platform1 = Platform()
+        self.platform2 = Platform()
+        self.player = Player()
     
-#    def test_gravity_not_pulling_when_player_is_on_platform(self):
-#        self.gravity(self.player, self.platform1, self.platform2)
-#        self.assertEqual(self.player.can_jump, True)
+    def test_gravity_not_pulling_when_player_is_on_platform(self):
+        Game.gravity(self.player, self.platform1, self.platform2, self.platform2)
+        self.assertEqual(self.player.can_jump, True)
